5664a62335f47b7830bce0a0f7f936a7d527ddbc
Adwaith <108259613+Proi5@users.noreply.github.com>
2022-06-28 16:55:45 +0530

    Delete Login directory

--- Login/First.py
+++
@@ -1,9 +0,0 @@
-
-
-print("To create a acoound type 1")
-print("To login type 2")
-Cho1 = input()
-if Cho1 == ("1"):
-    import Create
-if Cho1 == ("2"):
-    import login
--- Login/save.py
+++
@@ -1,4 +0,0 @@
-Name = "Adwaith"
-Number = "9074737176.0"
-Id = "Trigary"
-Password = "1232"
